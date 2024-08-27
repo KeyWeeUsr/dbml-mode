e6400c22c8a05003e8888bc7deb6218c35d5aa25
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2024-08-27 21:13:18 +0200

    [ADD] Test for single-line comment `//`

---
+++ dbml-mode-tests.el
@@ -0,0 +1,24 @@
+;;; dbml-mode-tests.el -- tests for dbml-mode
+
+;;; Code:
+
+(require 'ert)
+(require 'dbml-mode)
+
+(defsubst dbml-mode-in-ert ()
+  "Because something is turned off in ERT."
+  (dbml-mode)
+  (font-lock-ensure))
+
+(ert-deftest dbml-mode-comment-single-no-newline ()
+  "'// comment' with no newline is highlighted as a comment."
+  (with-temp-buffer
+    (insert "// comment")
+    (should-not (text-properties-at (point-min)))
+    (dbml-mode-in-ert)
+    (should (string= (format "%s" (text-properties-at (point-min)))
+                     "(face font-lock-comment-face)"))))
+
+(provide 'dbml-mode-tests)
+
+;;; dbml-mode-tests.el ends here
