7e438a831e4335d88272362116462ce3f49c3d23
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2024-09-05 00:48:20 +0200

    [REF] Better comparison in tests

--- dbml-mode-tests.el
+++ dbml-mode-tests.el
@@ -16,8 +16,9 @@
     (insert "// comment")
     (should-not (text-properties-at (point-min)))
     (dbml-mode-in-ert)
-    (should (string= (format "%S" (buffer-string))
-                     "#(\"// comment\" 0 10 (face font-lock-comment-face))"))))
+    (let ((expected #("// comment" 0 10 (face font-lock-comment-face))))
+      (should (string= (format "%S" (buffer-string))
+                       (format "%S" expected))))))
 
 (ert-deftest dbml-mode-comment-single-with-newline ()
   "'// comment' with newline is highlighted as a comment."
@@ -25,9 +26,9 @@
     (insert "// comment\n")
     (should-not (text-properties-at (point-min)))
     (dbml-mode-in-ert)
-    (should
-     (string= (format "%S" (buffer-string))
-              "#(\"// comment\n\" 0 11 (face font-lock-comment-face))"))))
+    (let ((expected #("// comment\n" 0 11 (face font-lock-comment-face))))
+      (should
+       (string= (format "%S" (buffer-string)) (format "%S" expected))))))
 
 (ert-deftest dbml-mode-comment-multi-no-newline ()
   "'/* comment */' with no newline is highlighted as a comment."
@@ -35,10 +36,10 @@
     (insert "/*\nsome\ncomment\n*/")
     (should-not (text-properties-at (point-min)))
     (dbml-mode-in-ert)
-    (should
-     (string=
-      (format "%S" (buffer-string))
-      "#(\"/*\nsome\ncomment\n*/\" 0 18 (face font-lock-comment-face))"))))
+    (let ((expected #("/*\nsome\ncomment\n*/" 0 18
+                      (face font-lock-comment-face))))
+      (should
+       (string= (format "%S" (buffer-string)) (format "%S" expected))))))
 
 (provide 'dbml-mode-tests)
 
