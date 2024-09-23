e1d109f910e131242acdbbae7241510bfb070ded
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2024-09-23 23:57:53 +0200

    [REF] Package commentary section

--- dbml-mode.el
+++ dbml-mode.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2024 Peter Badida
 
 ;; Author: Peter Badida <keyweeusr@gmail.com>
-;; Keywords: convenience, dbml, language, markup, highlight
+;; Keywords: convenience, dbml, language, markup, highlight, dbdiagram, diagram
 ;; Version: 1.0.0
 ;; Package-Requires: ((emacs "24.4"))
 ;; Homepage: https://github.com/KeyWeeUsr/dbml-mode
@@ -26,6 +26,10 @@
 ;; This major mode attempts to port all of the syntax highlighting from
 ;; https://dbdiagram.io and build upon it by providing helpers such as
 ;; duplicate checking and rendering SVGs directly in Emacs.
+
+;; For rendering, available as a key binding, it utilizes `dbml-renderer' which
+;; by default is provided by building a Docker image.  Check `dbml'
+;; customization group to adjust this or other functionality.
 
 ;;; Code:
 
